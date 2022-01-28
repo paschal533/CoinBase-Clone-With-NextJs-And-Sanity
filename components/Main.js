f6cfbf9832d552a4cb0c744c3b05c44bed67f30c
unknown <okwuosahpaschal@gmail.com>
2022-01-28 23:26:06 +0100

    third commit

--- components/Main.js
+++ components/Main.js
@@ -18,16 +18,4 @@ function Main({ thirdWebTokens, sanityTokens, walletAddress }) {
 
 export default Main;
 
-const Wrapper = styled.div`
-  display: flex;
-  max-height: calc(100vh - 64px);
-  overflow: hidden;
-  overflow-y: scroll;
-  :webkit-scrollbar {
-    display: none;
-  }
-
-  & div {
-    border-radius: 0.4rem;
-  }
-`;
+ 
